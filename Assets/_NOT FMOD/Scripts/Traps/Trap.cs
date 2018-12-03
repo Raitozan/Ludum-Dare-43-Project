1c23e915b6985b7b7a48d50703d8ff538966e566
Nathan Desages <nathan.desages@gmail.com>
2018-12-04 00:30:54 +0100

    added trap system

---
+++ Assets/_NOT FMOD/Scripts/Traps/Trap.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trap : MonoBehaviour {
+
+	public Vector3 movement;
+	public float speed;
+	public int damage;
+	public float lifeTime;
+	
+	// Update is called once per frame
+	void Update () {
+		if(lifeTime != -1)
+		{
+			lifeTime -= Time.deltaTime;
+			if (lifeTime <= 0)
+				Destroy(gameObject);
+		}
+		Vector3 velocity = movement * speed * Time.deltaTime;
+
+		transform.Translate(velocity);
+	}
+
+	private void OnTriggerEnter2D(Collider2D collision)
+	{
+		if (collision.CompareTag("Player"))
+		{
+			Player p = collision.GetComponent<Player>();
+			if (!p.isDashing)
+			{
+				GameManager.instance.playerEnergy -= damage;
+				Destroy(gameObject);
+			}
+		}
+	}
+}
---
+++ Assets/_NOT FMOD/Scripts/Traps/TrapCreator.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrapCreator : MonoBehaviour {
+
+	public GameObject trapPrefab;
+
+	public float instantiateTime;
+	float timer;
+
+	// Use this for initialization
+	void Start () {
+		timer = instantiateTime;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		timer -= Time.deltaTime;
+		if (timer <= 0)
+		{
+			Instantiate(trapPrefab, transform.position, Quaternion.identity, this.transform);
+			timer = instantiateTime;
+		}
+	}
+}
