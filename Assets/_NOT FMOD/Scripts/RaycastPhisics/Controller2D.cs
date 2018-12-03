76bc01237df0911941f34b0271b340e0df284199
Nathan Desages <nathan.desages@gmail.com>
2018-12-03 02:31:10 +0100

    added moving platform, but doesn't work correctly

--- Assets/_NOT FMOD/Scripts/RaycastPhisics/Controller2D.cs
+++ Assets/_NOT FMOD/Scripts/RaycastPhisics/Controller2D.cs
@@ -2,29 +2,16 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent (typeof(BoxCollider2D))]
-public class Controller2D : MonoBehaviour
+public class Controller2D : RaycastController
 {
-	public LayerMask collisionMask;
-
-	const float skinWidth = .015f;
-	public int horizontalRayCount = 4;
-	public int verticalRayCount = 4;
-
 	public float maxClimbAngle;
 	public float maxDescendAngle;
 
-	float horizontalRaySpacing;
-	float verticalRaySpacing;
-
-	public BoxCollider2D boxCollider;
-	RaycastOrigins raycastOrigins;
 	public CollisionsInfo collisions;
 
-	void Start ()
+	public override void Start()
 	{
-		boxCollider = GetComponent<BoxCollider2D> ();
-		CalculateRaySpacing();
+		base.Start();
 	}
 
 	public void Move(Vector3 velocity)
@@ -200,35 +187,6 @@ public class Controller2D : MonoBehaviour
 			CalculateRaySpacing();
 			isCrouching = false;
 		}
-	}
-
-	void UpdateRaycastOrigins()
-	{
-		Bounds bounds = boxCollider.bounds;
-		bounds.Expand(skinWidth * -2);
-
-		raycastOrigins.bottomLeft = new Vector2(bounds.min.x, bounds.min.y);
-		raycastOrigins.bottomRight = new Vector2(bounds.max.x, bounds.min.y);
-		raycastOrigins.topLeft = new Vector2(bounds.min.x, bounds.max.y);
-		raycastOrigins.topRight = new Vector2(bounds.max.x, bounds.max.y);
-	}
-
-	public void CalculateRaySpacing()
-	{
-		Bounds bounds = boxCollider.bounds;
-		bounds.Expand(skinWidth * -2);
-
-		horizontalRayCount = Mathf.Clamp(horizontalRayCount, 2, int.MaxValue);
-		verticalRayCount = Mathf.Clamp(verticalRayCount, 2, int.MaxValue);
-
-		horizontalRaySpacing = bounds.size.y / (horizontalRayCount - 1);
-		verticalRaySpacing = bounds.size.y / (verticalRayCount - 1);
-	}
-
-	struct RaycastOrigins
-	{
-		public Vector2 topLeft, topRight;
-		public Vector2 bottomLeft, bottomRight;
 	}
 
 	public struct CollisionsInfo
