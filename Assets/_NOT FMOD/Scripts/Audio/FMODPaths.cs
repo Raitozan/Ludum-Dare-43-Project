6e49d0b61ef11d8ec1bd5619ad62be99470e8607
Alessandro <alessandro.fama@ymail.com>
2018-12-03 13:03:11 +0100

    Updated FMOD banks and paths to events

--- Assets/_NOT FMOD/Scripts/Audio/FMODPaths.cs
+++ Assets/_NOT FMOD/Scripts/Audio/FMODPaths.cs
@@ -5,9 +5,20 @@
 
     public static readonly string ROBOT_JUMP = "event:/robot/jump";
     public static readonly string ROBOT_MOVE = "event:/robot/Footsteps_Floor";
+    public static readonly string ROBOT_DJ = "event:/robot/doublejump";
+    public static readonly string ROBOT_DASH = "event:/robot/dash";
+    public static readonly string ROBOT_BURN = "event:/robot/burn";
+
+    //level1
+    public static readonly string DOOR_SLIDE = "event:/game/level_1/door";
+
+    //ambience
+    public static readonly string AMBIENCE = "event:/ambience";
 
 
     // PARAMETERS
+
+    public static readonly string LEVEL = "Level";
 
     // SNAPSHOTS
 
